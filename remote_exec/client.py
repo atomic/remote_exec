e000316c6a55b87ac1551542abc0b2861f109d5d
Tony Lim <atomictheorist@gmail.com>
2017-03-22 17:13:14 -0700

    add command parameter for client code

--- remote_exec/client.py
+++ remote_exec/client.py
@@ -1,16 +1,36 @@
+#!/usr/bin/env python
 import xmlrpc.client
-import sys
+import sys, getopt
 
+
+# Client machine: ./client --server <server ip> --user <username> --password <password> --exec <program to execute>
 
 def main(argv):
-    if argv is None:
-        argv = sys.argv
-    ip = argv[1]
-    port = argv[2]
+    global command, ip
+    try:
+        opts, args = getopt.getopt(argv[1:], "hsupx", ["server=", "user=", "password=", "exec="])
+    except getopt.GetoptError:
+        print('./client --server <server ip> --user <username> --password <password> --exec <program to execute>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('test.py -i <inputfile> -o <outputfile>')
+            sys.exit()
+        elif opt in ("-s", "--server"):
+            ip = arg
+        elif opt in ("-u", "--user"):
+            user = arg
+        elif opt in ("-p", "--password"):
+            password = arg
+        elif opt in ("-x", "--exec"):
+            command = arg
 
-    with xmlrpc.client.ServerProxy("http://"+ip+":"+port) as proxy:
-        result = proxy.execute('ls')
+    port = "8888"
+
+    with xmlrpc.client.ServerProxy("http://" + ip + ":" + port) as proxy:
+        result = proxy.execute(command)
         print(result)
+
 
 if __name__ == "__main__":
     main(sys.argv)
